2fa537102a47c5a60a7b55a0b13258ba39741ba3
thatsOven <68780017+thatsOven@users.noreply.github.com>
2022-03-31 12:28:29 +0200

    (hopefully) fix runner and builder

--- runner/build.py
+++ runner/build.py
@@ -2,7 +2,7 @@ from PyInstaller.__main__ import run
 from distutils.dir_util   import copy_tree
 from shutil               import rmtree
 from pathlib              import Path
-from os                   import chdir, path, getcwd
+from os                   import chdir, path, getcwd, remove
 
 spath = str(Path(__file__).parent.absolute())
 chdir(spath)
@@ -12,13 +12,13 @@ run((
     "--icon=icon.ico",
     "--workpath=tmp",
     "--distpath=run",
-    "--specpath=tmp",
     "--clean",
     "run.py"
 ))
 
 rmtree("tmp")
 rmtree("__pycache__")
+remove("run.spec")
 
 chdir("..")
 copy_tree(path.join(spath, "run"), getcwd())
