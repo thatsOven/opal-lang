0fbf601c305b58dba20074985288ebf6f634e07a
thatsOven <68780017+thatsOven@users.noreply.github.com>
2022-06-04 23:13:05 +0200

    use ABC instead of ABCMeta
    the programmer can now specify a custom metaclass when using abstract classes

--- opalc.py
+++ opalc.py
@@ -326,11 +326,11 @@ class Compiler:
 
                             if nextAbstract:
                                 nextAbstract = False
-                                args += ",metaclass=_ABSTRACT_BASE_CLASS_"
+                                args += ",_ABSTRACT_BASE_CLASS_"
                         else:
                             if nextAbstract:
                                 nextAbstract = False
-                                args = "metaclass=_ABSTRACT_BASE_CLASS_"
+                                args = "_ABSTRACT_BASE_CLASS_"
                             else:
                                 args = ""
 
@@ -472,7 +472,7 @@ class Compiler:
                 charPtr += 9
 
                 if not self.imports["abstract"]:
-                    self.out = "from abc import abstractmethod\nfrom abc import ABCMeta as _ABSTRACT_BASE_CLASS_\n" + self.out
+                    self.out = "from abc import abstractmethod\nfrom abc import ABC as _ABSTRACT_BASE_CLASS_\n" + self.out
                     self.imports["abstract"] = True
 
                 nextAbstract = True
